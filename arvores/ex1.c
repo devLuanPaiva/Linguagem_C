054dbc33a3d3ea265c56c2d65603eacae163dee3
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-03-08 12:13:23 -0300

    aprendendo arvores em c

---
+++ arvores/ex1.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+    int conteudo;
+    struct no *esquerda, *direita;
+} No;
+
+typedef struct{
+    No *raiz;
+}ArvB;
+
+void inserirEsquerda(No *no, int valor){
+    if(no->esquerda == NULL){
+        No *novo = (No*)malloc(sizeof(No));
+        novo->conteudo = valor;
+        novo->esquerda = NULL;
+        novo->direita = NULL;
+        no->esquerda = novo;
+    }else{
+        if(valor < no->esquerda->conteudo){
+            inserirEsquerda(no->esquerda, valor);
+        }
+        if(valor > no->esquerda->conteudo){
+            inserirDireita(no->esquerda, valor);
+        }
+    }
+
+}
+void inserirDireita(No *no, int valor){
+    if(no->direita == NULL){
+        No *novo = (No*)malloc(sizeof(No));
+        novo->conteudo = valor;
+        novo->esquerda = NULL;
+        novo->direita = NULL;
+        no->direita = novo;
+    }else{
+        if(valor > no->direita->conteudo){
+            inserirDireita(no->direita, valor);
+        }else if(valor < no->direita->conteudo){
+            inserirEsquerda(no->direita, valor);
+        }
+    }
+}
+void inserir(ArvB *arv, int valor){
+    if(arv -> raiz == NULL){
+        No *novo = (No*)malloc(sizeof(No));
+        novo->conteudo = valor;
+        novo->esquerda = NULL;
+        novo->direita = NULL;
+        arv->raiz = novo;
+    }else {
+        if(valor < arv->raiz->conteudo){
+            inserirEsquerda(arv->raiz, valor);
+        }else if(valor > arv->raiz->conteudo){
+            inserirDireita(arv->raiz, valor);
+        }
+    }
+}
+void imprimir(No *raiz){
+    if(raiz != NULL){
+        printf("%d", raiz->conteudo);
+        imprimir(raiz->esquerda);
+        imprimir(raiz->direita);
+    }
+}
+int main(){
+    int op, valor;
+    ArvB arv;
+    arv.raiz = NULL;
+
+    do{
+        printf("\n0 - Sair\n1 - Inserir\n2 - Imprimir\n");
+        scanf("%d", &op);
+
+        switch(op){
+            case 0:
+                printf("\nSaindo...\n");
+                break;
+            case 1:
+                printf("Digite um valor: ");
+                scanf("%d", &valor);
+                inserir(&arv, valor);
+                break;
+            case 2:
+                printf("\nImprimindo a arvore:\n");
+                imprimir(arv.raiz);
+                break;
+            default:
+                printf("\nOpção inválida..\n");
+                break;
+        }
+    }while(op != 0);
+    return 0;
+}
